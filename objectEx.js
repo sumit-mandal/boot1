b30b54155413fccc3fa11316a50ad1635b6ee1f2
Sumit Mandal <63957082+sumit-mandal@users.noreply.github.com>
2020-11-16 23:13:36 +0530

    objectEx

---
+++ objectEx.js
@@ -0,0 +1,27 @@
+// var employee={
+//   name:"John Smith",
+//   job:"Programmer",
+//   age:31,
+//   nameLength:function(){
+//     console.log(this.name.length);
+//   }
+// }
+
+// var employee={
+//   name:'John Smith',
+//   job:'Programmer',
+//   age:31,
+//   report:function(){
+//     alert("Name is"+this.name+", job is "+this.job", Age is: "+this.age);
+//   }
+// }
+
+var employee={
+  name:'John Smith',
+  job:'Programmer',
+  age:31,
+  lastName:function(){
+    console.log(this.name.split(" ")[1])
+  }
+
+}
